fc625748b8767022dedbf31354079740b6f7b535
TEGARmine <tegarp00@gmail.com>
2022-07-29 17:10:50 +0700

    pake react

---
+++ src/components/Banner.js
@@ -0,0 +1,15 @@
+import { Container } from "react-bootstrap";
+
+export const Banner = () => {
+    return (
+        <section className="banner" id="home">
+            <Container>
+                <row>
+                    <col>
+
+                    </col>
+                </row>
+            </Container>
+        </section>
+    )
+}
